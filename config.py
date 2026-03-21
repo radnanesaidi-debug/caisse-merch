2554e6f6c0aaa59ecbf59007f0c6737c63dd9694
radnanesaidi-debug <r.adnanesaidi@gmail.com>
2026-03-21 21:52:20 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,3 @@ STAND_NAMES = ["Stand VVIP", "VIP", "ZONE 2"]
 SPREADSHEET_NAME = "Caisse_Merchandising"
 SHEET_SALES = "Ventes"
 SHEET_PRODUCTS = "Produits"
-
-# Produits par défaut
-DEFAULT_PRODUCTS = [
-    {"name": "Tracksuit Black", "price": 650, "sizes": ["S", "M", "L", "XL", "XXL"], "emoji": "⬛"},
-    {"name": "Ana Wydadia", "price": 200, "sizes": ["S", "M", "L", "XL"], "emoji": "🔴"},
-    {"name": "Never Split Tee", "price": 250, "sizes": ["S", "M", "L", "XL"], "emoji": "👕"},
-    {"name": "Bob", "price": 150, "sizes": ["UNI"], "emoji": "🧢"},
-    {"name": "Scarf", "price": 100, "sizes": ["UNI"], "emoji": "🧣"}
-]
