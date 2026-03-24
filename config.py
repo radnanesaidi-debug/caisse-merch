12d0f52f1005c119d6f10c14d84407fac6abb8c5
radnanesaidi-debug <r.adnanesaidi@gmail.com>
2026-03-24 11:28:40 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 APP_TITLE = "CAISSE WAC BRAND"
-STAND_NAMES = ["Stand VVIP", "VIP", "ZONE 2"]
+STAND_NAMES = ["Premium", "VIP", "ZONE 2"]
 PAYMENT_MODES = ["ESPECE", "TPE"] # Nouveau
 SPREADSHEET_NAME = "Caisse_Merchandising"
 SHEET_SALES = "Ventes"
